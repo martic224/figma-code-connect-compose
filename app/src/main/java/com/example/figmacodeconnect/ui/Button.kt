bb25a84b31186ad20b35c15c11b99a06d850f690
Craig Martin <martic224@medtronic.com>
2024-07-19 08:11:21 -0500

    Actually add the button

---
+++ app/src/main/java/com/example/figmacodeconnect/ui/Button.kt
@@ -0,0 +1,22 @@
+package com.example.figmacodeconnect.ui
+
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+import com.figma.code.connect.FigmaConnect
+
+@Composable
+fun MyButton(
+    text: String
+) {
+    Button(onClick = { /*TODO*/ }) {
+        
+    }
+}
+
+@FigmaConnect(url = "https://www.figma.com/design/4bMt5JcjzY8zWs00FARsMw/Material-3-Design-Kit-(Community)?node-id=53923-27633&m=dev")
+class ButtonDoc {
+    @Composable
+    fun ButtonSnippet() {
+        MyButton(text = "Hello World")
+    }
+}
